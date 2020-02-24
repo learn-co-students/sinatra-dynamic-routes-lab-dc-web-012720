abe96446aa0782147e7771bb635b4a7f8923593e
rjwilson0531 <rjwilson0531@gmail.com>
2020-02-23 19:20:27 -0500

    Done.

--- app.rb
+++ app.rb
@@ -7,12 +7,14 @@ class App < Sinatra::Base
   end
 
   get '/square/:number' do
-    @number = params[:number]
+    @number = params[:number].to_i
+    erb :square
   end
 
   get '/say/:number/:phrase' do
-    @number = params[:number]
+    @number = params[:number].to_i
     @phrase = params[:phrase]
+    erb :say
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
@@ -21,10 +23,14 @@ class App < Sinatra::Base
     @word3 = params[:word3]
     @word4 = params[:word4]
     @word5 = params[:word5]
+    erb :say
   end
 
-  get '/operation/:number1/:number2' do
-    @number1 = params[:number1]
-    @number2 = params[:number2]
+  get '/:operation/:number1/:number2' do
+    @operation = params[:operation]
+    @number1 = params[:number1].to_i
+    @number2 = params[:number2].to_i
+    erb :operation
+ 
   end
 end
